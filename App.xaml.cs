bb68807c41f2a87382bbb3a8aba7ecab05c7992b
Spik <89948736+Thespiki@users.noreply.github.com>
2025-03-05 21:27:42 +0100

    Base of the app

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace The_Network_Project;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
